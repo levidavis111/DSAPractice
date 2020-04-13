5cd1a802e2de4bdecfc3a37efce79d9505b72dd1
levidavis111 <52180404+levidavis111@users.noreply.github.com>
2020-04-13 16:35:44 -0400

    Update hammingDistance.swift

--- leetCode/hammingDistance.swift
+++ leetCode/hammingDistance.swift
@@ -25,3 +25,36 @@ func hammingDistance(_ x: Int, _ y: Int) -> Int {
     return result
 }
 */
+
+/*
+Another Approach:
+class Solution {
+    func hammingDistance(_ x: Int, _ y: Int) -> Int {
+        var count: Int = 0
+        var diff = x^y
+        
+        while diff != 0 {
+            count += diff & 1
+            diff = diff >> 1
+        }
+        
+        return count
+    }
+}
+
+And another:
+
+func hammingDistance(_ x: Int, _ y: Int) -> Int {
+    var x = x
+    var y = y
+    var result = 0
+    
+    while x > 0 || y > 0 {
+        result += (x % 2) ^ (y % 2)
+        
+        x = x >> 1
+        y = y >> 1
+    }
+    return result
+}
+*
