525907026e7c3916315f3bd328bf8eebf9949d94
levidavis111 <52180404+levidavis111@users.noreply.github.com>
2020-04-14 16:17:04 -0400

    Create invertBinaryTree.swift

---
+++ leetCode/invertBinaryTree.swift
@@ -0,0 +1,46 @@
+/*
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+class Solution {
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        guard root != nil else {return nil}
+        
+        let right = invertTree(root?.right)
+        let left = invertTree(root?.left)
+        
+        root?.left = right
+        root?.right = left
+        
+        return root
+    }
+}
