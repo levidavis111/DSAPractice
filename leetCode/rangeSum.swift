8b00b04db3429048e3073c3be60c404f6975e58b
levidavis111 <52180404+levidavis111@users.noreply.github.com>
2020-05-01 15:58:01 -0400

    Update rangeSum.swift

--- leetCode/rangeSum.swift
+++ leetCode/rangeSum.swift
@@ -65,3 +65,42 @@ class Solution {
         return sum
     }
 }
+//Recurrsive is much faster, but memory usage is much higher
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    var sum: Int = 0
+    func rangeSumBST(_ root: TreeNode?, _ L: Int, _ R: Int) -> Int {
+        // guard root != nil else {return 0}
+        
+        let value = root?.val ?? 0
+        
+        if value >= L && value <= R {
+            sum += value
+        }
+        
+        if let left = root?.left {
+            rangeSumBST(left, L, R)
+        }
+        
+        if let right = root?.right {
+            rangeSumBST(right, L, R)
+        }
+        
+        return sum
+    }
+}
