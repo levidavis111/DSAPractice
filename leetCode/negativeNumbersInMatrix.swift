5839d7d2928e9882f5d9f079b3d105aa10e97f2c
levidavis111 <52180404+levidavis111@users.noreply.github.com>
2020-05-05 15:34:59 -0400

    Update negativeNumbersInMatrix.swift

--- leetCode/negativeNumbersInMatrix.swift
+++ leetCode/negativeNumbersInMatrix.swift
@@ -48,3 +48,42 @@ class Solution {
         return count
     }
 }
+// Binary Search
+class Solution {
+    func countNegatives(_ grid: [[Int]]) -> Int {
+       var count: Int = 0
+        
+        for row in grid {
+            guard let index = findFirstNegative(row) else {continue}
+            count += row.count - index
+        }
+        return count
+    }
+    
+    func findFirstNegative(_ arr: [Int]) -> Int? {
+        guard arr[arr.count - 1] < 0 else {return nil}
+        if arr[0] < 0 {return 0}
+        
+        var left = 0
+        var right = arr.count - 1
+        
+        while left < right {
+            let middle = (left + right) / 2
+            let number = arr[middle]
+            guard middle > 0 else {
+                return middle + 1
+            }
+            let previousNumber = arr[middle - 1]
+            if number < 0 && previousNumber >= 0 {
+                return middle
+            } else if number >= 0 {
+                left = middle + 1
+            } else if number < 0 {
+                right = middle - 1
+            }
+        }
+        
+        return left
+        
+    }
+}
