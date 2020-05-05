1dacedbf796cb098a3e0c06e0f9ce682089e5c56
levidavis111 <52180404+levidavis111@users.noreply.github.com>
2020-05-05 14:15:33 -0400

    Create replaceElementWithGreatestElementRight.swift

---
+++ leetCode/replaceElementWithGreatestElementRight.swift
@@ -0,0 +1,40 @@
+/*
+1299. Replace Elements with Greatest Element on Right Side
+
+Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+After doing so, return the array.
+
+Example 1:
+
+Input: arr = [17,18,5,4,6,1]
+Output: [18,6,6,6,1,-1]
+ 
+
+Constraints:
+
+1 <= arr.length <= 10^4
+1 <= arr[i] <= 10^5
+
+*/
+class Solution {
+    func replaceElements(_ arr: [Int]) -> [Int] {
+        var output = [Int]()
+        
+        for index in 0..<arr.count {
+            guard index < arr.count - 1 else {
+                output.append(-1)
+                return output
+            }
+            
+            let subrange = arr[(index + 1)...]
+            let biggest = subrange.max() ?? 0
+            
+            output.append(biggest)
+            
+            
+        }
+        
+        return output
+    }
+}
