226a57de71af7f8e34cefd9d7c6dd690ac973f5d
levidavis111 <52180404+levidavis111@users.noreply.github.com>
2020-05-28 11:25:36 -0400

    Create sqrtX.swift

---
+++ leetCode/sqrtX.swift
@@ -0,0 +1,37 @@
+/*
+69. Sqrt(x)
+
+Implement int sqrt(int x).
+
+Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+
+Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
+
+Example 1:
+
+Input: 4
+Output: 2
+Example 2:
+
+Input: 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since 
+             the decimal part is truncated, 2 is returned.
+*/
+//Naive:
+class Solution {
+    func mySqrt(_ x: Int) -> Int {
+        guard x > 1 else {return x}
+        
+        for num in 2...x {
+            if num * num == x {
+                return num
+            } else {
+                if num * num > x {
+                    return num - 1
+                }
+            }
+        }
+        return 0
+    }
+}
