b40a10bfefcbf98df02fa13047d5ac0eb7a97bc8
levidavis111 <52180404+levidavis111@users.noreply.github.com>
2020-06-08 13:45:59 -0400

    Create queueReconstruction.swift

---
+++ leetCode/queueReconstruction.swift
@@ -0,0 +1,35 @@
+/*
+406. Queue Reconstruction by Height
+
+Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
+
+Note:
+The number of people is less than 1,100.
+
+ 
+Example
+
+Input:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+Output:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+*/
+class Solution {
+    func reconstructQueue(_ people: [[Int]]) -> [[Int]] {
+        let sortedArr = people.sorted(by: {
+            if $0[0] == $1[0] {
+                return $0[1] < $1[1]
+            } else {
+                return $0[0] > $1[0]
+            }
+        })
+        var output = [[Int]]()
+        for arr in sortedArr {
+            let place = arr[1]
+            output.insert(arr, at: place)
+        }
+        
+        return output
+    }
+}
