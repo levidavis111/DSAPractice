5c5ee6eefeac87ce586aa66ba3b9f36b2bf4103f
levidavis111 <52180404+levidavis111@users.noreply.github.com>
2020-06-08 12:09:41 -0400

    Create powerOfTwo.swift

---
+++ leetCode/powerOfTwo.swift
@@ -0,0 +1,32 @@
+/*
+231. Power of Two
+
+Given an integer, write a function to determine if it is a power of two.
+
+Example 1:
+
+Input: 1
+Output: true 
+Explanation: 20 = 1
+Example 2:
+
+Input: 16
+Output: true
+Explanation: 24 = 16
+Example 3:
+
+Input: 218
+Output: false
+*/
+class Solution {
+    func isPowerOfTwo(_ n: Int) -> Bool {
+        guard n > 0 else {return false}
+        
+        var sum = 1
+        
+        while sum < n {
+            sum *= 2
+        }
+        return sum == n
+    }
+}
