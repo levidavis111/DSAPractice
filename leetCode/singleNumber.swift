9ed0fd28574e8fddf04061d182df4e0209308677
levidavis111 <52180404+levidavis111@users.noreply.github.com>
2020-06-11 11:54:31 -0400

    Create singleNumber.swift

---
+++ leetCode/singleNumber.swift
@@ -0,0 +1,32 @@
+/*
+136. Single Number
+
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4
+*/
+class Solution {
+    func singleNumber(_ nums: [Int]) -> Int {
+        var seen = Set<Int>()
+        
+        for num in nums {
+            if seen.contains(num) {
+                seen.remove(num)
+            } else {
+                seen.insert(num)
+            }
+        }
+        return Array(seen)[0]
+    }
+}
