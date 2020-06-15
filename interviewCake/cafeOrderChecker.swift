d76e8bd14f32aa5d353e742973ab6996950825d3
levidavis111 <52180404+levidavis111@users.noreply.github.com>
2020-06-15 17:41:31 -0400

    Update cafeOrderChecker.swift

--- interviewCake/cafeOrderChecker.swift
+++ interviewCake/cafeOrderChecker.swift
@@ -12,69 +12,26 @@ The dine-in orders as they were entered into the system and given to the kitchen
 Each customer order (from either register) as it was finished by the kitchen. (servedOrders)
 Given all three arrays, write a function to check that my service is first-come, first-served. All food should come out in the same order customers requested it.
 
-We'll represent each customer order as a unique integer.
+We'll represent each customer order as a unique integer. Integer values are arbitrary.
 */
 
 func isFirstComeFirstServed(takeOutOrders: [Int], dineInOrders: [Int], servedOrders: [Int]) -> Bool {
-
-    // var takeOutIndex = 0
-    // var dineInIndex = 0
-    // let takeOutMax = takeOutOrders.count - 1
-    // let dineInMax = dineInOrders.count - 1
+    var takeOutIndex = 0
+    var dineInIndex = 0
+    let takeOutMax = takeOutOrders.count - 1 
+    let dineInMax = dineInOrders.count - 1
     
-    // for order in servedOrders {
-    //     if takeOutIndex <= takeOutMax && order == takeOutOrders[takeOutIndex] {
-    //         takeOutIndex += 1
-    //     } else if dineInIndex <= dineInMax && order == dineInOrders[dineInIndex] {
-    //         dineInIndex += 1
-    //     } else {
-    //         return false
-    //     }
-        
-    // }
-    
-    // if dineInIndex != dineInOrders.count || takeOutIndex != takeOutOrders.count {
-    //         return false
-    //     }
-         
-    //     return true
-   
-    // if servedOrders.count == 0 {
-    //     return true
-    // }
-    
-    // if takeOutOrders.count > 0 && takeOutOrders[0] == servedOrders[0] {
-    //     return isFirstComeFirstServed(takeOutOrders:Array(takeOutOrders[1...]), 
-    //                                                     dineInOrders: dineInOrders, 
-    //                                                     servedOrders: Array(servedOrders[1...]) )
-    // } else if dineInOrders.count > 0 && dineInOrders[0] == servedOrders[0] {
-    //     return isFirstComeFirstServed(takeOutOrders: takeOutOrders,
-    //                                 dineInOrders: Array(dineInOrders[1...]),
-    //                                 servedOrders: Array(servedOrders[1...]))
-    // } else {
-    //     return false
-    // }
-
-    var mergedOrders = Array(repeating: 0, count: takeOutOrders.count + dineInOrders.count)
-    var currentTakeOutIndex = 0
-    var currentDineInIndex = 0
-    var currentMergedIndex = 0
-    
-    while currentMergedIndex < mergedOrders.count {
-        let isTakeOutExhausted = currentTakeOutIndex >= takeOutOrders.count
-        let isDineInExhausted = currentDineInIndex >= dineInOrders.count
-        
-        if !isTakeOutExhausted && (isDineInExhausted || takeOutOrders[currentTakeOutIndex] <= dineInOrders[currentDineInIndex]) {
-            mergedOrders[currentMergedIndex] = takeOutOrders[currentTakeOutIndex]
-            currentTakeOutIndex += 1
+    for order in servedOrders {
+        if takeOutIndex <= takeOutMax && takeOutOrders[takeOutIndex] == order {
+            takeOutIndex += 1
+        } else if dineInIndex <= dineInMax && dineInOrders[dineInIndex] == order {
+            dineInIndex += 1
         } else {
-            mergedOrders[currentMergedIndex] = dineInOrders[currentDineInIndex]
-            currentDineInIndex += 1
+            return false
         }
-        
-        currentMergedIndex += 1
     }
-    
-
-    return servedOrders == mergedOrders
+    if dineInIndex != dineInOrders.count || takeOutIndex != takeOutOrders.count {
+        return false
+    }
+    return true
 }
