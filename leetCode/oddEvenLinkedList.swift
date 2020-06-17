34f86dbcadfcc02978afe2647361b8fd91368f62
levidavis111 <52180404+levidavis111@users.noreply.github.com>
2020-06-17 11:33:06 -0400

    Create oddEvenLinkedList.swift

---
+++ leetCode/oddEvenLinkedList.swift
@@ -0,0 +1,53 @@
+/*
+328. Odd Even Linked List
+
+Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+
+You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+Example 1:
+
+Input: 1->2->3->4->5->NULL
+Output: 1->3->5->2->4->NULL
+Example 2:
+
+Input: 2->1->3->5->6->4->7->NULL
+Output: 2->3->6->7->1->5->4->NULL
+ 
+
+Constraints:
+
+The relative order inside both the even and odd groups should remain as it was in the input.
+The first node is considered odd, the second node even and so on ...
+The length of the linked list is between [0, 10^4].
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func oddEvenList(_ head: ListNode?) -> ListNode? {
+        guard head != nil else {return nil}
+        var currentOdd = head
+        var evenHead: ListNode? = head?.next
+        var currentEven: ListNode? = evenHead
+        
+        while currentEven != nil && currentEven?.next != nil {
+            currentOdd?.next = currentOdd?.next?.next
+            currentEven?.next = currentEven?.next?.next
+            currentOdd = currentOdd?.next
+            currentEven = currentEven?.next
+        }
+        
+        currentOdd?.next = evenHead
+        evenHead = nil
+        
+        return head
+    }
+}
