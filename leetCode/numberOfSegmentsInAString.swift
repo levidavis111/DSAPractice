877ed2b649f0731dd49e9c7fd0b9f9987d78073a
levidavis111 <52180404+levidavis111@users.noreply.github.com>
2020-07-13 15:56:42 -0400

    Create numberOfSegmentsInAString.swift

---
+++ leetCode/numberOfSegmentsInAString.swift
@@ -0,0 +1,17 @@
+/*
+434. Number of Segments in a String
+
+Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
+
+Please note that the string does not contain any non-printable characters.
+
+Example:
+
+Input: "Hello, my name is John"
+Output: 5
+*/
+class Solution {
+    func countSegments(_ s: String) -> Int {
+        return s.split{$0.isWhitespace}.count
+    }
+}
