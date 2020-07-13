52ca104298d9f5a78118bb7c54011d2b6d3e85d1
levidavis111 <52180404+levidavis111@users.noreply.github.com>
2020-07-13 15:58:26 -0400

    Create pathSumIII.swift

---
+++ leetCode/pathSumIII.swift
@@ -0,0 +1,64 @@
+/*
+437. Path Sum III
+
+You are given a binary tree in which each node contains an integer value.
+
+Find the number of paths that sum to a given value.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+
+The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+
+Example:
+
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+Return 3. The paths that sum to 8 are:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3. -3 -> 11
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func pathSum(_ root: TreeNode?, _ sum: Int) -> Int {
+        guard let root = root else {return 0}
+        
+        return findPath(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum)
+    }
+       
+    func findPath(_ root: TreeNode?, _ sum: Int) -> Int {
+        guard let root = root else {return 0}
+        var result = 0
+        if sum == root.val {
+            result += 1
+        }
+        
+        result += findPath(root.left, sum - root.val)
+        result += findPath(root.right, sum - root.val)
+        
+        return result
+    }
+}
