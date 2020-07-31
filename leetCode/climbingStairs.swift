65d67720f1a2cc8e5f60e957cbdc91ba29d6cd8f
levidavis111 <52180404+levidavis111@users.noreply.github.com>
2020-07-31 12:13:22 -0400

    Update climbingStairs.swift

--- leetCode/climbingStairs.swift
+++ leetCode/climbingStairs.swift
@@ -40,3 +40,21 @@ class Solution {
         
     }
 }
+
+class Solution {
+    func climbStairs(_ n: Int) -> Int {
+        
+        guard n >= 3 else {return n}
+        
+        var a = 1
+        var b = 2
+        var c = 0
+        
+        for _ in 2..<n {
+            c = a + b
+            a = b
+            b = c
+        }
+        return c
+    }
+}
