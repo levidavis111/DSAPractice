fff5a0f640bcfe5455ebfdbc2d920e5cb6ed3d0c
levidavis111 <52180404+levidavis111@users.noreply.github.com>
2020-09-01 13:36:56 -0400

    Update isValidAnagram.swift

--- leetCode/isValidAnagram.swift
+++ leetCode/isValidAnagram.swift
@@ -51,3 +51,48 @@ class Solution {
         return true
     }
 }
+//Another solution, not really faster:
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        guard s.count == t.count else {return false}
+        
+        let sArr = Array(s)
+        let tArr = Array(t)
+        
+        var sHash = [Character:Int]()
+        var tHash = [Character:Int]()
+        
+        for index in 0..<sArr.count {
+            let sChar = sArr[index]
+            let tChar = tArr[index]
+            
+            if let sValue = sHash[sChar] {
+                sHash[sChar] = sValue + 1
+            } else {
+                sHash[sChar] = 1
+            }
+            
+            if let tValue = tHash[tChar] {
+                tHash[tChar] = tValue + 1
+            } else {
+                tHash[tChar] = 1
+            }
+        }
+        
+        for char in s {
+            if let sValue = sHash[char] {
+                if let tValue = tHash[char] {
+                   if sValue != tValue {
+                       return false
+                   } 
+                } else {
+                    return false
+                } 
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+}
