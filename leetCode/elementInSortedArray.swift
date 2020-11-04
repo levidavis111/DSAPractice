be78a9fcef2e8737daf83ba96ec19fc2b3f811ac
levidavis111 <52180404+levidavis111@users.noreply.github.com>
2020-11-04 13:38:56 -0500

    Create elementInSortedArray.swift

---
+++ leetCode/elementInSortedArray.swift
@@ -0,0 +1,39 @@
+/*
+1287. Element Appearing More Than 25% In Sorted Array
+
+Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time.
+
+Return that integer.
+
+Example 1:
+
+Input: arr = [1,2,2,6,6,6,6,7,10]
+Output: 6
+ 
+
+Constraints:
+
+1 <= arr.length <= 10^4
+0 <= arr[i] <= 10^5
+*/
+class Solution {
+    func findSpecialInteger(_ arr: [Int]) -> Int {
+        var hashMap = [Int:Int]()//[num:occurances]
+        
+        for num in arr {
+            if let value = hashMap[num] {
+                hashMap[num] = value + 1
+                if (value + 1) * 4 > arr.count {return num}
+            } else {
+                hashMap[num] = 1
+            }
+        }
+        
+        for (key, value) in hashMap {
+            if value * 4 > arr.count {
+                return key
+            }
+        }
+        return Int.min
+    }
+}
