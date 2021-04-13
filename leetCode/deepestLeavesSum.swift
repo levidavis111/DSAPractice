da37b47981c7cd8192c410f226a9752e17eba202
levidavis111 <52180404+levidavis111@users.noreply.github.com>
2021-04-13 14:09:23 -0400

    Update deepestLeavesSum.swift

--- leetCode/deepestLeavesSum.swift
+++ leetCode/deepestLeavesSum.swift
@@ -84,3 +84,76 @@ class Solution {
         return sum
     }
 }
+//Slightly faster using binary search:
+
+struct NodeDepth {
+    let node: TreeNode
+    let depth: Int
+    var val: Int {
+        return node.val
+    }
+}
+
+class Solution {
+    func deepestLeavesSum(_ root: TreeNode?) -> Int {
+        guard let root = root else {return 0}
+        let rootND = NodeDepth(node: root, depth: 1)
+        var leaves = [NodeDepth]()
+        var stack = [NodeDepth]()
+        stack.append(rootND)
+        var sum = 0
+        
+        while !stack.isEmpty {
+            let currentND = stack.removeLast()
+            let node = currentND.node
+            let depth = currentND.depth
+            
+            if node.left == nil && node.right == nil {
+                insertInPlace(currentND, &leaves)
+            } else {
+                if let left = node.left {
+                    let nd = NodeDepth(node: left, depth: depth + 1)
+                    stack.append(nd)
+                }
+                
+                if let right = node.right {
+                    let nd = NodeDepth(node: right, depth: depth + 1)
+                    stack.append(nd)
+                }
+            }
+        }
+        
+        // let sortedLeaves = leaves.sorted(by: {$0.depth < $1.depth})
+        let deepest = leaves[leaves.count - 1].depth
+        leaves.forEach {
+            if $0.depth == deepest {
+                sum += $0.val
+            }
+        }
+        
+        return sum
+    }
+    
+    private func insertInPlace(_ node: NodeDepth, _ arr: inout [NodeDepth]) {
+        guard arr.count > 0 else {arr.append(node); return}
+        
+        var left = 0
+        var right = arr.count - 1
+        
+        while left <= right {
+            let middle = (left + right) / 2
+            let currentNode = arr[middle]
+            let depthVal = currentNode.depth
+            
+            if node.depth == depthVal {
+                arr.insert(node, at: middle)
+                return
+            } else if node.depth > depthVal {
+                left = middle + 1
+            } else {
+                right = middle - 1
+            }
+        }
+        arr.insert(node, at: left)
+    }
+}
